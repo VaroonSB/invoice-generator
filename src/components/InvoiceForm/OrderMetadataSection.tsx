d1fd266b97a4bb66385bf8e3c4c056d83610facd
VaroonSB <varoonsb1311@gmail.com>
2025-02-18 12:21:37 +0530

    created order metadata section, refactored file path usage, date format

---
+++ src/components/InvoiceForm/OrderMetadataSection.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { RefObject } from "react";
+import { Input } from "../Input";
+
+interface OrderMetadataSectionProps {
+  refs: {
+    despatchThroughRef: RefObject<HTMLInputElement | null>;
+    orderThroughRef: RefObject<HTMLInputElement | null>;
+    orderDateRef: RefObject<HTMLInputElement | null>;
+  };
+}
+
+export const OrderMetadataSection = ({ refs }: OrderMetadataSectionProps) => {
+  const { despatchThroughRef, orderThroughRef, orderDateRef } = refs;
+  return (
+    <section className="flex flex-col gap-4 mb-6 bg-gray-200 p-4 rounded-3xl">
+      <h3 className="text-xl font-semibold mb-2 text-gray-600">
+        Order Metadata
+      </h3>
+      <Input title="Despatch Through" type="text" ref={despatchThroughRef} />
+      <Input title="Order Through" type="text" ref={orderThroughRef} />
+      <Input title="Invoice Date" type="date" ref={orderDateRef} />
+    </section>
+  );
+};
