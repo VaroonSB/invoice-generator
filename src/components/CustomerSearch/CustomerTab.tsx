8b5de2b44d3c93d029f5a706c81618e6eab70f26
VaroonSB <varoonsb1311@gmail.com>
2025-02-21 11:20:05 +0530

    count logic added for customer list

--- src/components/CustomerSearch/CustomerTab.tsx
+++ src/components/CustomerSearch/CustomerTab.tsx
@@ -6,23 +6,26 @@ export const CustomerTab = ({
   customerList: Array<Customer>;
 }) => {
   return (
-    <section className="flex flex-wrap gap-5 bg-white p-4 rounded-3xl shadow-md">
-      {customerList?.map((customer, index) => (
-        <section
-          key={index}
-          className="flex flex-col gap-2 bg-gray-100 p-4 cursor-pointer hover:scale-105 rounded-3xl w-[32%] shadow-lg"
-          onClick={() => {
-            // TODO: do some
-          }}
-        >
-          <div className="font-bold text-lg">{customer.customerName}</div>
-          <div className="text-sm text-gray-600">
-            {customer.addressLine1} {customer.addressLine2}{" "}
-            {customer.addressLine3}
-          </div>
-          <div className="text-sm text-gray-500">{customer.customerGst}</div>
-        </section>
-      ))}
-    </section>
+    <div className="flex flex-col gap-4">
+      <div className="px-4 text-lg font-semibold">Total: {customerList.length}</div>
+      <section className="flex flex-wrap gap-5 bg-white p-4 rounded-3xl shadow-md">
+        {customerList?.map((customer, index) => (
+          <section
+            key={index}
+            className="flex flex-col gap-2 bg-gray-100 p-4 cursor-pointer transition-transform hover:scale-105 rounded-3xl w-[32.36%] shadow-xl"
+            onClick={() => {
+              // TODO: do some
+            }}
+          >
+            <div className="font-bold text-lg">{customer.customerName}</div>
+            <div className="text-sm text-gray-600">
+              {customer.addressLine1} {customer.addressLine2}{" "}
+              {customer.addressLine3}
+            </div>
+            <div className="text-sm text-gray-500">{customer.customerGst}</div>
+          </section>
+        ))}
+      </section>
+    </div>
   );
 };
