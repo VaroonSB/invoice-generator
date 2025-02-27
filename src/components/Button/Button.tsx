3bf186b3287923e6a294afe380a07a3335f75918
VaroonSB <varoonsb1311@gmail.com>
2025-02-27 21:48:10 +0530

    Button component refactored and reused

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,21 @@
 import { MouseEventHandler } from "react";
 
 interface ButtonProps {
+  type?: "submit" | "button";
   label: string;
   classNames?: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button = ({ label, classNames = "", onClick }: ButtonProps) => {
+export const Button = ({
+  type = "button",
+  label,
+  classNames = "",
+  onClick,
+}: ButtonProps) => {
   return (
     <button
-      type="button"
+      type={type}
       className={
         "px-6 py-3 bg-gradient-to-r from-blue-500 to-blue-800 text-white font-semibold rounded-2xl shadow-lg transform transition-transform hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 " +
         classNames
--- src/components/InvoiceForm/InvoiceForm.tsx
+++ src/components/InvoiceForm/InvoiceForm.tsx
@@ -8,14 +8,21 @@ import { OrderMetadataSection } from "./OrderMetadataSection";
 import { useInvoice } from "@/context/InvoiceContext";
 import { useCustomer } from "@/hooks/useCustomer";
 import { INITIAL_INVOICE_VALUES } from "@/context/InvoiceContext/constants";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "../Button";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export const InvoiceForm = () => {
   const { formData, createInvoice, generatePdf, setFormData, operation } =
     useInvoice();
   const { createCustomer } = useCustomer();
 
+  const [print, setPrint] = useState<boolean>(false);
+
   const router = useRouter();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_host, _path, year, month, invoiceName] = usePathname().split("/");
 
   const submitHandler = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -40,9 +47,12 @@ export const InvoiceForm = () => {
     if (status === "error") {
       return;
     }
-    await generatePdf();
-    setFormData(INITIAL_INVOICE_VALUES);
-    router.push("/");
+    const { status: generationStatus } = await generatePdf();
+    setPrint(true);
+    if (generationStatus === "error") {
+      setPrint(false);
+      toast.warning("PDF not generated. Hence, Print is not available");
+    }
   };
 
   return (
@@ -70,12 +80,34 @@ export const InvoiceForm = () => {
           </div>
         </div>
 
-        <button
-          type="submit"
-          className="mt-2 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-600 transform hover:scale-105 transition-transform duration-300 ease-in-out w-2/3"
-        >
-          Submit
-        </button>
+        {print ? (
+          <div className="grid grid-cols-2 gap-4">
+            <Button
+              type="button"
+              label="Close"
+              classNames="mt-2 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-600 transform hover:scale-105 transition-transform duration-300 ease-in-out"
+              onClick={() => {
+                setFormData(INITIAL_INVOICE_VALUES);
+                router.push("/");
+              }}
+            />
+            <Button
+              type="button"
+              label="Print"
+              classNames="mt-2 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-600 transform hover:scale-105 transition-transform duration-300 ease-in-out"
+              onClick={() => {
+                setFormData(INITIAL_INVOICE_VALUES);
+                router.push(`/invoice/pdf/${year}/${month}/${invoiceName}`);
+              }}
+            />
+          </div>
+        ) : (
+          <Button
+            type="submit"
+            label="Submit"
+            classNames="mt-2 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full shadow-lg hover:from-blue-600 hover:to-purple-600 transform hover:scale-105 transition-transform duration-300 ease-in-out"
+          />
+        )}
       </form>
     </div>
   );
